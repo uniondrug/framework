e6cfc47baac7a40763cb65737444fd7372b3647c
倪述荣 <xueron@xueron.com>
2018-01-11 13:43:17 +0800

    支持默认配置。

--- src/Providers/ConfigProvider.php
+++ src/Providers/ConfigProvider.php
@@ -18,18 +18,30 @@ class ConfigProvider implements ServiceProviderInterface
             'config',
             function () {
                 $env = $this->environment();
-                $config = [];
+                $config = new Config([]);
                 $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->configPath()), \RecursiveIteratorIterator::SELF_FIRST);
                 foreach ($iterator as $item) {
                     if (Text::endsWith($item, '.php', false)) {
                         $name = str_replace([$this->configPath() . DIRECTORY_SEPARATOR, '.php'], '', $item);
                         $data = include $item;
-                        if (is_array($data) && isset($data[$env])) {
-                            $config[$name] = $data[$env];
+
+                        // 默认配置组
+                        if (is_array($data) && isset($data['default']) && is_array($data['default'])) {
+                            $config[$name] = $data['default'];
+                        }
+
+                        // 非空初始化
+                        if (!isset($config[$name])) {
+                            $config[$name] = [];
+                        }
+
+                        // 指定环境的配置组，覆盖默认配置
+                        if (is_array($data) && isset($data[$env]) && is_array($data[$env])) {
+                            $config->merge(new Config([$name => $data[$env]]));
                         }
                     }
                 }
-                return new Config($config);
+                return $config;
             }
         );
     }
