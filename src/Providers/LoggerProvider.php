b47b9e192ca45b3e7d49bb7994cb98d7b30c02f0
付义兵 <websearch@163.com>
2018-06-15 13:38:16 +0800

    Revert "更新Logger日志, 支持豪秒级记录"
    This reverts commit 366dd3b1d22bbd56ee4cdc2e1abda9c9ea47de50.

--- src/Providers/LoggerProvider.php
+++ src/Providers/LoggerProvider.php
@@ -1,51 +1,45 @@
 <?php
 /**
- * @date 2018-06-15
+ * 日志服务注册
+ *
  */
+
 namespace Uniondrug\Framework\Providers;
 
 use Phalcon\Di\ServiceProviderInterface;
-use Phalcon\Logger;
 use Phalcon\Logger\Adapter\File;
-use Uniondrug\Framework\Container;
 
-/**
- * 日志注入Provider
- * @package Uniondrug\Framework\Providers
- */
 class LoggerProvider implements ServiceProviderInterface
 {
-    /**
-     * @inheritdoc
-     */
     public function register(\Phalcon\DiInterface $di)
     {
-        $di->set('logger', function($logCategory = 'app'){
-            $month = date('Y-m');
-            $date = date('Y-m-d');
-            /**
-             * @var Container $this
-             */
-            if ($this->getConfig()->path('logger.splitDir', false)) {
-                $logPath = $this->logPath().'/'.$logCategory.'/'.$month;
-                $logFile = $logPath.'/'.$date.'.log';
-            } else {
-                $logPath = $this->logPath().'/'.$logCategory;
-                $logFile = $logPath.'/'.$date.'.log';
-            }
-            try {
-                if (!file_exists($logPath)) {
-                    mkdir($logPath, 0755, true);
+        // logger
+        $di->set(
+            'logger',
+            function ($logCategory = 'app') {
+                $month = date('Y-m');
+                $date = date('Y-m-d');
+                if (\config()->path('logger.splitDir', false)) {
+                    $logPath = \app()->logPath() . '/' . $logCategory . '/' . $month;
+                    $logFile = $logPath . '/' . $date . '.log';
+                } else {
+                    $logPath = \app()->logPath() . '/' . $logCategory;
+                    $logFile = $logPath . '/' . $date . '.log';
                 }
-            } catch(\Throwable $e) {
-                // skip. multi process may try to make dir at the same time. just skip errors.
+                try {
+                    if (!file_exists($logPath)) {
+                        mkdir($logPath, 0755, true);
+                    }
+                } catch (\Throwable $e) {
+                    // skip. multi process may try to make dir at the same time. just skip errors.
+                }
+                $logLevel = \config()->path('logger.level', \Phalcon\Logger::DEBUG);
+
+                $logger = new File($logFile);
+                $logger->setLogLevel($logLevel);
+
+                return $logger;
             }
-            $logLevel = $this->getConfig()->path('logger.level', Logger::DEBUG);
-            $formatter = new LoggerFormatter();
-            $logger = new File($logFile);
-            $logger->setLogLevel($logLevel);
-            $logger->setFormatter($formatter);
-            return $logger;
-        });
+        );
     }
 }
