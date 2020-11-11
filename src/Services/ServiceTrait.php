93409ceeb2f3a49cc7cfae1b9e04efd4a1e1c1fb
Haven <chenhao@uniondrug.com>
2020-11-11 14:48:12 +0800

    Update ServiceTrait.php

--- src/Services/ServiceTrait.php
+++ src/Services/ServiceTrait.php
@@ -3,28 +3,28 @@
  * @author wsfuyibing <websearch@163.com>
  * @date   2018-03-23
  */
-
 namespace Uniondrug\Framework\Services;
 
 /**
- * @property \GuzzleHttp\Client                      $httpClient
- * @property \Phalcon\Cache\BackendInterface         $cache
- * @property \Phalcon\Config                         $config
- * @property \Phalcon\Db\AdapterInterface            $dbSlave
- * @property \Phalcon\Logger\AdapterInterface        $logger
- * @property \Redis                                  $redis
- * @property \Uniondrug\Redis\RedisLock              $redisLock
- * @property \Uniondrug\Framework\Container          $di
- * @property \Uniondrug\Crontab\Crontab              $crontabService
- * @property \Uniondrug\Middleware\MiddlewareManager $middlewareManager
- * @property \Uniondrug\Register\RegisterClient      $registerClient
- * @property \Uniondrug\Service\Server               $serviceServer
- * @property \Uniondrug\Service\Client               $serviceClient
- * @property \Uniondrug\TcpClient\Client             $tcpClient
- * @property \Uniondrug\Trace\TraceClient            $traceClient
- * @property \Uniondrug\Server\Task\Dispatcher       $taskDispatcher
- * @property \Uniondrug\ServiceSdk\ServiceSdk        $serviceSdk
- * @property \Uniondrug\Validation\Param             $validationService
+ * @property \GuzzleHttp\Client                        $httpClient
+ * @property \Phalcon\Cache\BackendInterface           $cache
+ * @property \Phalcon\Config                           $config
+ * @property \Phalcon\Db\AdapterInterface              $dbSlave
+ * @property \Phalcon\Logger\AdapterInterface          $logger
+ * @property \Redis                                    $redis
+ * @property \Uniondrug\Redis\RedisLock                $redisLock
+ * @property \Uniondrug\Framework\Container            $di
+ * @property \Uniondrug\Crontab\Crontab                $crontabService
+ * @property \Uniondrug\Middleware\MiddlewareManager   $middlewareManager
+ * @property \Uniondrug\Register\RegisterClient        $registerClient
+ * @property \Uniondrug\Service\Server                 $serviceServer
+ * @property \Uniondrug\Service\Client                 $serviceClient
+ * @property \Uniondrug\TcpClient\Client               $tcpClient
+ * @property \Uniondrug\Trace\TraceClient              $traceClient
+ * @property \Uniondrug\Server\Task\Dispatcher         $taskDispatcher
+ * @property \Uniondrug\ServiceSdk\ServiceSdk          $serviceSdk
+ * @property \Uniondrug\Validation\Param               $validationService
+ * @property \Uniondrug\CoinSignature\SignatureService $signatureService
  */
 trait ServiceTrait
 {
